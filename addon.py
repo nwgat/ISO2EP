fd6e82a92a0468cd45b90a9751fe329049220b67
nwgat <github@nwgat.net>
2015-07-20 01:55:01 +0200

    Create addon.py
    early alpha test

---
+++ addon.py
@@ -0,0 +1,15 @@
+import xbmcaddon
+import xbmcgui
+addon       = xbmcaddon.Addon()
+
+# Read nfo file for episodes
+file = open('disc.nfo', 'r')
+print file.read()
+
+# Map episodes to file disc1.iso\BDMV\PLAYLIST\00051.mpls > S01E01 
+#todo
+
+# Play file (click S01E01 then play disc1.iso\BDMV\PLAYLIST\00051.mpls)
+#todo
+
+xbmcgui.Dialog().ok.print file.read()
